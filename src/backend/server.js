d0d5759e778c14084d13989bfb695a067415439e
devcenourinha <gabrielbuenotl2@gmail.com>
2025-08-22 14:58:57 -0300

    test: (api/echo e api/ping) validar POST, middleware express.json() e acessar ao req.body

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const { json } = express;
+const cors = require("cors");
 
 const app = express();
 const PORT = 5000;
+
+app.use(cors());           
+app.use(express.json());     
 
 app.use(json());
 
 app.get("/api/hello", (req, res) => {
   res.json({ message: "Olá do backend!" });
+});
+
+app.get("/api/ping", (req, res) => {
+  res.json({ ok: true, time: new Date().toISOString() });
+});
+
+app.post("/api/echo", (req, res) => {
+  res.json({ youSent: req.body });
 });
 
 app.listen(PORT, () => {
